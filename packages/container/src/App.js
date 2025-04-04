89ad32f0e8ec7438b62b7e8adc564486112853b0
Mauro <mauro_9_2@hotmail.com>
2025-04-03 22:06:04 -0500

    fixed aws secret access key.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <div>Hi there 🔥</div>
+      <div>Hi there 🔥😀</div>
       <hr />
       <MarketingApp />
     </div>
